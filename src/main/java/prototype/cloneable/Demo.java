4230c80b153a1280b4bf28af5e3b212c146b78a1
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-20 19:04:05 -0400

    implemented copy-constructor approach. Closes #3.

--- src/main/java/prototype/cloneable/Demo.java
+++ src/main/java/prototype/cloneable/Demo.java
@@ -1,4 +1,4 @@
-package prototype;
+package prototype.cloneable;
 
 
 import java.util.Arrays;
---
+++ src/main/java/prototype/copyconstructors/Demo.java
@@ -0,0 +1,72 @@
+package prototype.copyconstructors;
+
+
+
+class Address {
+	public String streetAddress, city, country;
+
+	public Address(String streetAddress, String city, String country) {
+		this.streetAddress = streetAddress;
+		this.city = city;
+		this.country = country;
+	}
+
+	//implement copy-constructor
+	//copy each field
+	public Address(Address other){
+		this(other.streetAddress, other.city, other.country);
+	}
+
+	@Override
+	public String toString() {
+		return "Address{" +
+				"streetAddress='" + streetAddress + '\'' +
+				", city='" + city + '\'' +
+				", country='" + country + '\'' +
+				'}';
+	}
+}
+
+class Employee{
+	public String name;
+	public Address address;
+
+	public Employee(String name, Address address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	//copy-constructor
+	public Employee(Employee other){
+		name = other.name;
+		address = new Address(other.address);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"name='" + name + '\'' +
+				", address=" + address +
+				'}';
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Address ukAddress = new Address("123 London Road", "London", "UK");
+		Employee john = new Employee("John", ukAddress);
+		//now we want a new employee who lives at the same ukAddress
+		Employee jane = new Employee("Jane", new Address(ukAddress));
+
+		//OR a new employee with same name as john but from Germany
+		//bad example ikr ;)
+		//anyways, the takeaway from here is, this copy-constructor approach is more powerful than cloneable interface approach
+		Employee germanJohn = new Employee(john);
+		germanJohn.address.country = "Germany";
+
+		System.out.println(john);
+		System.out.println(germanJohn);
+		System.out.println(jane);
+
+	}
+}
