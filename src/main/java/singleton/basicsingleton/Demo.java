1852b5bc0e7a49cc8571934c9f40766cc0f98676
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-25 07:01:49 -0400

    break singleton via reflection. Closes #16.

--- src/main/java/singleton/basicsingleton/Demo.java
+++ src/main/java/singleton/basicsingleton/Demo.java
@@ -1,5 +1,8 @@
 package singleton.basicsingleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * This class enforces the rule of having only one instance at any given time. In other words, we need to prevent creation of additional copies of our class.
  *
@@ -7,6 +10,10 @@ package singleton.basicsingleton;
  * Make the constructor private, restricting anyone to call constructor for object creation
  * Create a static final Instance variable which has reference to ONE object
  * Expose the INSTANCE field via getter method
+ *
+ * Problems: (How can we defeat singleton?)
+ * 1. Reflection
+ * 2. Serialization
  *
  */
 class BasicSingleton {
@@ -44,9 +51,26 @@ public class Demo {
 		BasicSingleton singletonInstance2 = BasicSingleton.getINSTANCE();
 		singletonInstance2.setValue(456);
 
+		//Breaking singleton via reflection
+		try{
+			Constructor[] constructors = BasicSingleton.class.getDeclaredConstructors();
+			for(Constructor constructor: constructors){
+				//below section will destroy our singleton contract
+				constructor.setAccessible(true);
+				singletonInstance1 = (BasicSingleton) constructor.newInstance();
+				singletonInstance1.setValue(123);
+				break;
+			}
+		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
 		//If our singleton pattern implementation logic is valid then instance1 and instance2 should point to the same object
 		//In other words, the recent changes to the object will be the one holding in the object
-		System.out.println("Instance 1 value : "+singletonInstance1.getValue());
-		System.out.println("Instance 2 value : "+singletonInstance2.getValue());
+		System.out.println("Instance 1 value : "+singletonInstance1.getValue()); //123
+		System.out.println("Instance 2 value : "+singletonInstance2.getValue()); //456
+
+		//Reflection can be solved by using enum type singleton
+
 	}
 }
