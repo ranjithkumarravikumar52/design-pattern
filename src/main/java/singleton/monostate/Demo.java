543864833ca8321cfc5b592df4358d9cc3111063
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-04-02 09:47:26 -0400

    implemented a monostate and listed its problems. Closes #14.

--- src/main/java/singleton/monostate/Demo.java
+++ src/main/java/singleton/monostate/Demo.java
@@ -2,17 +2,19 @@ package singleton.monostate;
 
 /**
  * Imagine in our company we have a CEO with some fields.
+ * Now, if we want to make our POJO instantly singleton. we have to change the fields to static.
+ * Now all the instances to a single storage element
  */
 class ChiefExecutiveOfficer{
-	private String name;
-	private int age;
+	private static String name;
+	private static int age;
 
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		ChiefExecutiveOfficer.name = name;
 	}
 
 	public int getAge() {
@@ -20,7 +22,7 @@ class ChiefExecutiveOfficer{
 	}
 
 	public void setAge(int age) {
-		this.age = age;
+		ChiefExecutiveOfficer.age = age;
 	}
 
 	@Override
@@ -32,4 +34,12 @@ class ChiefExecutiveOfficer{
 	}
 }
 public class Demo {
+	public static void main(String[] args) {
+		ChiefExecutiveOfficer chiefExecutiveOfficer = new ChiefExecutiveOfficer();
+		chiefExecutiveOfficer.setName("Adam Smith");
+		chiefExecutiveOfficer.setAge(55);
+
+		ChiefExecutiveOfficer chiefExecutiveOfficer1 = new ChiefExecutiveOfficer();
+		System.out.println(chiefExecutiveOfficer1); //already initialized, however no indication that this is already initialized
+	}
 }
