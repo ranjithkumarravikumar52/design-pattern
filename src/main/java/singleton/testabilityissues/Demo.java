5c11c8481ab5520baed2ae137c302e5d5032103d
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-04-02 10:27:41 -0400

    Closes #15.

--- src/main/java/singleton/testabilityissues/Demo.java
+++ src/main/java/singleton/testabilityissues/Demo.java
@@ -13,9 +13,14 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+interface Database{
+	int getPopulation(String name);
+}
 
 //load the capitals.text only ONCE
-class SingletonDatabase {
+class SingletonDatabase implements Database{
 	private static final SingletonDatabase INSTANCE = new SingletonDatabase();
 	private static int instanceCount = 0;
 	private Dictionary<String, Integer> capitals = new Hashtable<>();
@@ -56,6 +61,7 @@ class SingletonDatabase {
 		return INSTANCE;
 	}
 
+	@Override
 	public int getPopulation(String name){
 		return capitals.get(name);
 	}
@@ -72,17 +78,64 @@ class SingletonRecordFinder{
 	}
 }
 
+/**
+ * <b>True unit test case takes around 16ms, whereas IT takes around 63ms</b>
+ */
 class Tests{
-	@Test
 	/**
 	 * Problem is loading real database here. This is more of a ITTest
 	 */
+	@Test
 	public void singletonTotalPopulationTest(){
 		SingletonRecordFinder rf = new SingletonRecordFinder();
 		List <String> cities = Arrays.asList("Seoul", "Mexico City");
 		int tp = rf.getTotalPopulation(cities);
-		System.out.println(17500000+17400000 == tp); //this test is going to work
+		assertEquals(17500000+17400000, tp); //this test is going to work
 
+	}
+
+	//unit test
+	@Test
+	public void dependentPopulationTest(){
+		DummyDatabase dummyDatabase = new DummyDatabase();
+		ConfigurableRecordFinder configurableRecordFinder = new ConfigurableRecordFinder(dummyDatabase);
+		assertEquals(3, configurableRecordFinder.getTotalPopulation(
+				Arrays.asList("alpha", "beta")
+		));
+	}
+}
+
+class ConfigurableRecordFinder{
+	private Database database;
+
+	//injection
+	public ConfigurableRecordFinder(Database database) {
+		this.database = database;
+	}
+
+	//configurable to inject any DB we want (real or dummy)
+	public int getTotalPopulation(List<String> names){
+		int result = 0;
+		for(String name: names){
+			result += database.getPopulation(name);
+		}
+		return result;
+	}
+}
+
+class DummyDatabase implements Database{
+
+	private Dictionary<String, Integer> data = new Hashtable<>();
+
+	public DummyDatabase(){
+		data.put("alpha", 1);
+		data.put("beta", 2);
+		data.put("theta", 3);
+	}
+
+	@Override
+	public int getPopulation(String name) {
+		return data.get(name);
 	}
 }
 
