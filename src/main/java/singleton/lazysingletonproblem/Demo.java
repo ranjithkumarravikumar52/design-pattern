d95bb394983bd843bcc43a4ebaff7823a4f62687
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-04-02 09:04:09 -0400

    implemented lazy singleton and listed it's problems. #11.

---
+++ src/main/java/singleton/lazysingletonproblem/Demo.java
@@ -0,0 +1,31 @@
+package singleton.lazysingletonproblem;
+
+/**
+ * Lazy initialization: Sometimes we want the initialization(object creation) only when we call the
+ * get instance method. NOT in static block or method.
+ */
+class LazySingleton{
+	private static LazySingleton INSTANCE;
+	private LazySingleton(){
+		System.out.println("initializing a lazy singleton");
+	}
+
+	/**
+	 * We are waiting object creation till someone calls our getInstance().
+	 * however this might cause race condition. Imagine two threads checking null conditions at the same time and creates
+	 * two object.
+	 * Solution is to use synchronized modifier. However this will call cause performance issues.
+	 * Other approach is double-checked locking
+	 */
+	public static LazySingleton getINSTANCE(){
+		if(INSTANCE == null){
+			INSTANCE = new LazySingleton();
+		}
+		return INSTANCE;
+	}
+}
+
+
+
+public class Demo {
+}
